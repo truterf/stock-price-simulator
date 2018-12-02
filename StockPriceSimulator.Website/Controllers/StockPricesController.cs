660ec10748743cff7e054da22c558080a68756a1
francois <francois.truter@gmail.com>
2018-12-02 17:07:55 +1100

    Blank MVC website

---
+++ StockPriceSimulator.Website/Controllers/StockPricesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StockPriceSimulator.Website.Controllers
+{
+    public class StockPricesController : Controller
+    {
+        // GET: StockPrices
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StockPriceSimulator.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StockPriceSimulator.Website.MvcApplication" Language="C#" %>
